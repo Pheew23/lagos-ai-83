e51c71f96602b1c68d4ed7f147eea3364f63b305
Pheew23 <106793294+Pheew23@users.noreply.github.com>
2026-07-14 00:04:36 +0700

    Refactor app.py to improve structure and functionality

--- app.py
+++ app.py
@@ -94,7 +94,13 @@ st.markdown("""
 # --- API KEY YANG DISEMBATKAN ---
 API_KEY = "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"
 BASE_URL = "https://integrate.api.nvidia.com/v1"
-MODEL_NAME = "deepseek-ai/deepseek-v4-pro"
+MODEL_NAME = "mistralai/mistral-large-3-675b-instruct-2512
+
+Build
+
+Playground
+
+Model Card"
 
 # --- FUNGSI PEMBANTU (LOGIKA UTAMA TETAP SAMA) ---
 
