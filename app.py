083f68200c2860d18329b522c3480dddefcef745
Pheew23 <106793294+Pheew23@users.noreply.github.com>
2026-07-13 23:25:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ st.markdown("""
 # --- API KEY YANG DISEMBATKAN ---
 API_KEY = "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"
 BASE_URL = "https://integrate.api.nvidia.com/v1"
-MODEL_NAME = "openai/gpt-oss-120b"
+MODEL_NAME = "qwen/qwen3.5-397b-a17b"
 
 # --- FUNGSI PEMBANTU (LOGIKA UTAMA TETAP SAMA) ---
 
