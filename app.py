69ec9b1738110cf0179e4aa68922e827b663db70
Pheew23 <106793294+Pheew23@users.noreply.github.com>
2026-07-14 23:30:57 +0700

    Refactor app.py for improved functionality and styling

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ st.markdown("""
 # --- KONFIGURASI API ---
 API_KEY = "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" # Pindahkan ke st.secrets saat rilis produksi
 BASE_URL = "https://integrate.api.nvidia.com/v1"
-MODEL_NAME = "mistralai/ministral-14b-instruct-2512"
+MODEL_NAME = "llama-guard-4-12b"
 
 # --- FUNGSI PEMBANTU ---
 @st.cache_data(show_spinner=False)
