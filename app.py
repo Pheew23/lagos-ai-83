046c8bafff280c285bd8ab24a44a3c3082376325
Pheew23 <106793294+Pheew23@users.noreply.github.com>
2026-07-21 19:31:17 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.set_page_config(
 )
 
 # --- INIT COOKIE MANAGER ---
-@st.cache_resource(experimental_allow_widgets=True)
+@st.cache_resource
 def get_cookie_manager():
     return stx.CookieManager()
 
