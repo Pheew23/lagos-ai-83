bb955cf5753c2ed811bcd743b186b34291435dfa
Pheew23 <106793294+Pheew23@users.noreply.github.com>
2026-07-21 20:04:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ st.markdown("""
         
         /* Merapikan tulisan pada tombol sidebar agar tidak menabrak batas */
         [data-testid="stSidebar"] button p {
-            overflow: hidden;
+            overflow: nohidden;
             text-overflow: ellipsis;
             white-space: nowrap;
         }
