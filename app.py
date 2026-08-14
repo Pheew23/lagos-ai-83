f2ce9de8dcde64350dfd28c10fc2ce7948c99910
Pheew23 <106793294+Pheew23@users.noreply.github.com>
2026-08-14 19:04:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ API_KEY = st.secrets["NVIDIA_API_KEY"]
 BASE_URL = "https://integrate.api.nvidia.com/v1"
 
 MODEL_MAPPING = {
-    "minimaxai/minimax-m3": "1. Flash (text only)",
+    "minimaxai/minimax-m3": "1. Flash (Pro)",
     "google/diffusiongemma-26b-a4b-it": "2. Stable",
     "thinkingmachines/inkling": "3. Pro(text only)",
     "nvidia/nemotron-3-nano-omni-30b-a3b-reasoning": "4. Pro (Analisis)",
